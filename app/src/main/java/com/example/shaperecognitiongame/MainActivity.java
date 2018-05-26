9e35141ee59a7da34b656980d6906acf4afe09bb
emirhanaydin <emirhanaydin95@gmail.com>
2018-05-26 19:14:43 +0300

    Generate new shape on the camera activity result

--- app/src/main/java/com/example/shaperecognitiongame/MainActivity.java
+++ app/src/main/java/com/example/shaperecognitiongame/MainActivity.java
@@ -13,26 +13,40 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     public static final String VERTICES_EXTRA = "vertices_extra";
+    public static final int NEW_GAME_REQUEST = 100;
+
+    private int mVertices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Random random = new Random();
-        final int vertices = random.nextInt(6);
-
-        int shapeImage = ShapeHelper.getImageResource(vertices);
-        ImageView imageView = findViewById(R.id.imageView);
-        imageView.setImageResource(shapeImage);
+        chooseNewShape();
 
         final Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), CvCameraActivity.class);
-                intent.putExtra(VERTICES_EXTRA, vertices);
-                startActivity(intent);
+                intent.putExtra(VERTICES_EXTRA, mVertices);
+                startActivityForResult(intent, NEW_GAME_REQUEST);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == NEW_GAME_REQUEST) {
+            chooseNewShape();
+        }
+    }
+
+    private void chooseNewShape() {
+        Random random = new Random();
+        mVertices = random.nextInt(6);
+
+        int shapeImage = ShapeHelper.getImageResource(mVertices);
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(shapeImage);
     }
 }
