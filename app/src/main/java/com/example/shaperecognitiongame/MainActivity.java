8102ef7b8140faa9181a6e3a2d02ec248f0035ff
emirhanaydin <emirhanaydin95@gmail.com>
2018-05-27 13:51:04 +0300

    Create camera permission request structure

--- app/src/main/java/com/example/shaperecognitiongame/MainActivity.java
+++ app/src/main/java/com/example/shaperecognitiongame/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.shaperecognitiongame;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -10,9 +15,11 @@ import android.widget.ImageView;
 import com.example.shaperecognitiongame.shapes.ShapeHelper;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CAMERA = 0;
     public static final String VERTICES_EXTRA = "vertices_extra";
     public static final int NEW_GAME_REQUEST = 100;
 
+    private View mMainLayout;
     private int mVertices;
 
     @Override
@@ -20,14 +27,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mMainLayout = findViewById(R.id.main_layout);
         chooseNewShape();
 
         final Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), CvCameraActivity.class);
-                intent.putExtra(VERTICES_EXTRA, mVertices);
-                startActivityForResult(intent, NEW_GAME_REQUEST);
+                verifyCameraPermission();
             }
         });
     }
@@ -45,5 +51,67 @@ public class MainActivity extends AppCompatActivity {
         int shapeImage = ShapeHelper.getImageResource(mVertices);
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageResource(shapeImage);
+    }
+
+    private void startCamera() {
+        Intent intent = new Intent(getApplicationContext(), CvCameraActivity.class);
+        intent.putExtra(VERTICES_EXTRA, mVertices);
+        startActivityForResult(intent, NEW_GAME_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            // Request for camera permission.
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCamera();
+            } else {
+                // Permission request was denied.
+                Snackbar.make(mMainLayout, R.string.camera_access_required,
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    private void verifyCameraPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Permission is already available, start camera preview
+            startCamera();
+        } else {
+            // Permission is missing and must be requested.
+            requestCameraPermission();
+        }
+    }
+
+    /**
+     * Requests the {@link android.Manifest.permission#CAMERA} permission.
+     * If an additional rationale should be displayed, the user has to launch the request from
+     * a SnackBar that includes additional information.
+     */
+    private void requestCameraPermission() {
+        // Permission has not been granted and must be requested.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // Display a SnackBar with cda button to request the missing permission.
+            Snackbar.make(mMainLayout, R.string.camera_access_required,
+                    Snackbar.LENGTH_INDEFINITE).setAction(R.string.ok, new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Request the permission
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            PERMISSION_REQUEST_CAMERA);
+                }
+            }).show();
+
+        } else {
+            // Request the permission. The result will be received in onRequestPermissionResult().
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+        }
     }
 }
